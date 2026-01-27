3fb08d3d409ea513dd26bebede5677fe7a0a384e
ali-imtiyazkhan <itzkhantijara@gmail.com>
2026-01-27 23:48:56 +0530

    initial commit

---
+++ apps/backend/routes/admin.ts
@@ -0,0 +1,27 @@
+
+import { Router } from "express";
+import { adminMiddleware } from "../middleware/admin";
+
+const router = Router();
+
+router.post("/contest", adminMiddleware, (req, res) => {
+    const {offset, page} = req.query;
+
+})
+
+router.post("/challenge", adminMiddleware, (req, res) => {
+    const {offset, page} = req.query;
+
+})
+
+router.post("/link/:challengeId/:contestId", adminMiddleware, (req, res) => {
+    const {offset, page} = req.query;
+
+})
+
+router.delete("/link/:challengeId/:contestId", adminMiddleware, (req, res) => {
+    const {offset, page} = req.query;
+
+})
+
+export default router;
